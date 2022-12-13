1888972acc8229b8d09f45d44f571aefc6d48d59
Aleksey Grin <iamdasuper@gmail.com>
2022-12-13 10:50:09 +0300

    super step

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,7 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-  List<Widget> scoreKeeper = [
-
-  ];
+  List<Widget> scoreKeeper = [];
 
   @override
   Widget build(BuildContext context) {
